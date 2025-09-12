3e0fed619dd2c7632787e94766907f20f8d96336
qinbilei <qinbilei@liquidnetwork.com>
2025-09-12 12:16:59 +0800

    fix

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -65,7 +65,7 @@ class Settings(BaseSettings):
         "http://localhost",
     ]
 
-    @field_validator("BACKEND_CORS_ORIGINS", mode="before")
+    @field_validator("BACKEND_CORS_ORIGINS", "ALLOWED_HOSTS", mode="before")
     @classmethod
     def assemble_cors_origins(cls, v: str | list[str]) -> list[str] | str:
         if isinstance(v, str) and not v.startswith("["):
