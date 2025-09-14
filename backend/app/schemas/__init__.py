b656a74ec879924b00eecf53819ac95b8269c4b8
qinbilei <qinbilei@liquidnetwork.com>
2025-09-14 17:15:09 +0800

    fix

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -2,7 +2,7 @@
 from .user import (
     UserBase, UserResponse, UserCreate, UserUpdate, UserInDB
 )
-from .token import Token, TokenPayload
+from .token import Token, TokenPayload, TwoFactorResponse
 from .course import (
     CourseBase, CourseCreate, CourseUpdate, CourseResponse, CourseInDB
 )
